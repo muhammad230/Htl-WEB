e1edcd87c23b951474a56b02014321ae7721085e
muhammad230 <ali.khansmr3@gmail.com>
2026-04-22 12:39:12 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function App() {
+
+  return (
+   <div>
+    <h1 className='text-4xl text-blue-500 mt-1.5 text-center font-bold underline-black'>Welcome to Hotel Booking website</h1>
+
+   </div>
+  )
+}
+
+export default App
