9c7534b5f080ae93632ea11be56e3e3e9938eb82
muhammad230 <ali.khansmr3@gmail.com>
2026-04-22 15:39:20 +0500

    update

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import '../App.css'
+import React from "react";
+import "../App.css";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaUtensils } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+
+
 
 const FirstPage = () => {
   return (
-    <section className='mt-20'>
+    <section className="mt-17">
       <div className="text-center space-y-6">
         <h1 className="text-4xl font-semibold text-[#1a1a1a] tracking-tight leading-tight">
           Discover Restaurants
@@ -16,30 +20,35 @@ const FirstPage = () => {
 
       <div className="flex justify-center mt-8 mb-16">
         <div className="flex gap-6 flex-wrap items-center justify-center max-w-9xl w-full px-6 py-6">
-          
           {/* Search Input */}
           <div className="relative w-72">
             <FaMagnifyingGlass className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500" />
             <input
               type="text"
               placeholder="Search Instrument, Guitars..."
-              className="bg-white w-full py-4 pr-4 pl-10 border border-gray-300 rounded-lg text-base "
+              className="bg-white w-full py-4 pr-4 pl-10 border border-gray-300 rounded-lg  "
             />
           </div>
 
           {/* Category */}
-          <div className="w-50">
-            <select className="w-full py-4 px-6 border border-gray-300 rounded-lg bg-white ">
-              <option>Category</option>
-              <option>Guitar</option>
-              <option>Piano</option>
-              <option>Drums</option>
+          <div className="w-50 relative">
+            {/* Icon */}
+            <FaUtensils className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 pointer-events-none" />
+
+            <select className="w-full py-4 pl-10 pr-10 border border-gray-300 rounded-lg bg-white">
+              <option>Cuisines</option>
+              <option>Italian</option>
+              <option>Chinese</option>
+              <option>Pakistani</option>
             </select>
           </div>
 
           {/* Location */}
-          <div className="w-50">
-            <select className="w-full py-4 px-6 border border-gray-300 rounded-lg bg-white ">
+          <div className="w-50 relative">
+            {/* Icon */}
+            <FaLocationDot className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-500 pointer-events-none" />
+
+            <select className="w-full py-4 pl-12 pr-6 border border-gray-300 rounded-lg bg-white">
               <option>Location</option>
               <option>New York</option>
               <option>London</option>
@@ -51,11 +60,10 @@ const FirstPage = () => {
           <button className="px-6 py-3 rounded-lg text-white bg-gradient-to-r from-purple-600 to-pink-500">
             View Bookings
           </button>
-
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default FirstPage;
