a50188ca817619e414dcb0c8b31266c88e3d4446
muhammad230 <ali.khansmr3@gmail.com>
2026-04-22 14:51:04 +0500

    add search box or more changed

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,20 +1,61 @@
 import React from 'react'
 import '../App.css'
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
-const firstPage = () => {
+const FirstPage = () => {
   return (
-    // header of web
-<section className='mt-20'>
-  <div className="text-center space-y-6">
-    <h1 className="text-4xl font-semibold text-[#1a1a1a] font-gilroy tracking-tight leading-tight">
-      Discover Restaurants
-    </h1>
-    <p className="text-base text-[#4a4a4a] font-gilroy">
-      Find your perfect dining experience
-    </p>
-  </div>
-</section>
-    )
+    <section className='mt-20'>
+      <div className="text-center space-y-6">
+        <h1 className="text-4xl font-semibold text-[#1a1a1a] tracking-tight leading-tight">
+          Discover Restaurants
+        </h1>
+        <p className="text-base text-[#4a4a4a]">
+          Find your perfect dining experience
+        </p>
+      </div>
+
+      <div className="flex justify-center mt-8 mb-16">
+        <div className="flex gap-6 flex-wrap items-center justify-center max-w-9xl w-full px-6 py-6">
+          
+          {/* Search Input */}
+          <div className="relative w-72">
+            <FaMagnifyingGlass className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500" />
+            <input
+              type="text"
+              placeholder="Search Instrument, Guitars..."
+              className="bg-white w-full py-4 pr-4 pl-10 border border-gray-300 rounded-lg text-base "
+            />
+          </div>
+
+          {/* Category */}
+          <div className="w-50">
+            <select className="w-full py-4 px-6 border border-gray-300 rounded-lg bg-white ">
+              <option>Category</option>
+              <option>Guitar</option>
+              <option>Piano</option>
+              <option>Drums</option>
+            </select>
+          </div>
+
+          {/* Location */}
+          <div className="w-50">
+            <select className="w-full py-4 px-6 border border-gray-300 rounded-lg bg-white ">
+              <option>Location</option>
+              <option>New York</option>
+              <option>London</option>
+              <option>Dubai</option>
+            </select>
+          </div>
+
+          {/* Button */}
+          <button className="px-6 py-3 rounded-lg text-white bg-gradient-to-r from-purple-600 to-pink-500">
+            View Bookings
+          </button>
+
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default firstPage
+export default FirstPage;
